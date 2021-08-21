75da065ca254eafe11df822ec1c207e3b5ad42ab
Charnel-DevOPS <Charnelside@gmail.com>
2021-08-21 09:37:26 +0300

    Update PersistenceJPAConfig.java

--- src/main/java/spring/crud/config/PersistenceJPAConfig.java
+++ src/main/java/spring/crud/config/PersistenceJPAConfig.java
@@ -1,5 +1,6 @@
 package spring.crud.config;
 
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
